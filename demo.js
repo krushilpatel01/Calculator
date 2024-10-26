6eb84d3eb628f26f0f7122348d22be209ded3a54
kruhsilpatel01 <krushilchabhadiya600@gmail.com>
2024-10-26 09:43:49 +0530

    Publish form pc to github

---
+++ demo.js
@@ -0,0 +1,10 @@
+function myfunction() {
+    console.log("hello");
+    console.log("hello");
+}
+
+// document.getElementById('demo2').innerHTML = myfunction();
+
+const arrowmul = (a, b) => {
+    console.log(a * b);
+};
